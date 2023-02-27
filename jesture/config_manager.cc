c3e770acd22375ac58396fbeb1769daa88e70618
Carson Storm <carson.storm@outlook.com>
2023-02-27 00:12:48 -0700

    fix uninitialized pointer

--- jesture/config_manager.cc
+++ jesture/config_manager.cc
@@ -1,10 +1,11 @@
-#include <QtCore/QStandardPaths>
-#include <QtCore/QFile>
-#include <QtCore/QTextStream>
-#include <QtCore/QIODevice>
-#include <QtCore/QDir>
-#include <QtCore/QJsonDocument>
 #include "config_manager.h"
+
+#include <QtCore/QDir>
+#include <QtCore/QFile>
+#include <QtCore/QIODevice>
+#include <QtCore/QJsonDocument>
+#include <QtCore/QStandardPaths>
+#include <QtCore/QTextStream>
 
 const char* default_gestures = R"(
 [
@@ -32,6 +33,9 @@ const char* default_settings = R"(
 )";
 
 ConfigManager::ConfigManager() {
+    gestures = nullptr;
+    settings = nullptr;
+
     load_gestures();
     load_settings();
 }
@@ -42,42 +46,52 @@ void ConfigManager::save() {
 }
 
 void ConfigManager::write_gestures() {
-    QDir().mkpath(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation));
-    QFile* gestures_file = new QFile(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/gestures.json");
+    QDir().mkpath(
+        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation));
+    QFile* gestures_file = new QFile(
+        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) +
+        "/gestures.json");
     gestures_file->open(QIODevice::WriteOnly);
     QTextStream out(gestures_file);
     out << gestures->toJson();
 }
 
 void ConfigManager::write_settings() {
-    QDir().mkpath(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation));
-    QFile* settings_file = new QFile(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/settings.json");
+    QDir().mkpath(
+        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation));
+    QFile* settings_file = new QFile(
+        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) +
+        "/settings.json");
     settings_file->open(QIODevice::WriteOnly);
     QTextStream out(settings_file);
     out << settings->toJson();
 }
 
 void ConfigManager::load_gestures() {
-    QString app_data_path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    QString app_data_path =
+        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
     QFile* gestures_file = new QFile(app_data_path + "/gestures.json");
     if (gestures_file->exists()) {
         gestures_file->open(QIODevice::ReadOnly);
-        gestures = new QJsonDocument(QJsonDocument::fromJson(gestures_file->readAll()));
+        gestures = new QJsonDocument(
+            QJsonDocument::fromJson(gestures_file->readAll()));
     }
-    
+
     if (!gestures || gestures->toJson() == "") {
         gestures = new QJsonDocument(QJsonDocument::fromJson(default_gestures));
     }
 }
 
 void ConfigManager::load_settings() {
-    QString app_data_path = QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
+    QString app_data_path =
+        QStandardPaths::writableLocation(QStandardPaths::AppDataLocation);
     QFile* settings_file = new QFile(app_data_path + "/settings.json");
     if (settings_file->exists()) {
         settings_file->open(QIODevice::ReadOnly);
-        settings = new QJsonDocument(QJsonDocument::fromJson(settings_file->readAll()));
+        settings = new QJsonDocument(
+            QJsonDocument::fromJson(settings_file->readAll()));
     }
-    
+
     if (!settings || settings->toJson() == "") {
         settings = new QJsonDocument(QJsonDocument::fromJson(default_settings));
     }
--- jesture/jesture.cc
+++ jesture/jesture.cc
@@ -1,9 +1,10 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QSystemTrayIcon>
-#include "main_window.h"
+
 #include "config_manager.h"
 #include "jesture/jesturepipe/controller.h"
 #include "jesture/jesturepipe/settings.h"
+#include "main_window.h"
 #include "tools/cpp/runfiles/runfiles.h"
 
 /*
@@ -39,51 +40,51 @@ JesturePipeInit getInit(Runfiles *runfiles) {
     std::string landmark_model_lite_path = runfiles->Rlocation(
         "mediapipe/mediapipe/modules/hand_landmark/hand_landmark_lite.tflite");
 
-    return JesturePipeInit{
-        palm_model_full_path,
-        palm_model_lite_path,
-        landmark_model_full_path,
-        landmark_model_lite_path
-    };
+    return JesturePipeInit{palm_model_full_path, palm_model_lite_path,
+                           landmark_model_full_path, landmark_model_lite_path};
 }
 
 int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
-    
+
     QApplication app(argc, argv);
     app.setApplicationName("Jesture");
-    
+
     auto app_icon = new QIcon("icons/settings.svg");
     app.setWindowIcon(*app_icon);
-    
+
     auto config_manager = new ConfigManager();
-    config_manager->connect(&app, &QApplication::aboutToQuit, config_manager, &ConfigManager::save);
-    
+    config_manager->connect(&app, &QApplication::aboutToQuit, config_manager,
+                            &ConfigManager::save);
+
     std::string error;
     Runfiles *runfiles = Runfiles::Create(argv[0], &error);
-    
+
     if (runfiles == nullptr) {
-        std::cout << error << std::endl;
+        std::cout << "Error: " << error << std::endl;
         return 1;
     }
-    
-    auto jesturepipe_controller = new JesturePipeController(getInit(runfiles), &app);
-    jesturepipe_controller->connect(&app, &QApplication::aboutToQuit, jesturepipe_controller, &JesturePipeController::Stop);
-    
+
+    auto jesturepipe_controller =
+        new JesturePipeController(getInit(runfiles), &app);
+    jesturepipe_controller->connect(&app, &QApplication::aboutToQuit,
+                                    jesturepipe_controller,
+                                    &JesturePipeController::Stop);
+
     delete runfiles;
-    
-    JesturePipeSettings settings {
+
+    JesturePipeSettings settings{
         .camera_index = 0,
         .mode = 1,
         .num_hands = 2,
     };
-    
+
     jesturepipe_controller->Start(settings);
-    
+
     auto window = new MainWindow(jesturepipe_controller);
     window->connect(window, &MainWindow::quit, &app, &QApplication::quit);
     window->resize(1280, 720);
     window->show();
-    
+
     return app.exec();
 }
